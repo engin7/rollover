7a736d72cbd0e1893b353bb0cb4a7095b82cd112
Engin KUK <kuk.engin@gmail.com>
2020-04-27 01:16:35 +0300

    height adjusted.

--- Devreden/SuperTableViewController.swift
+++ Devreden/SuperTableViewController.swift
@@ -79,6 +79,16 @@ class SuperTableViewController: UITableViewController {
         }
     }
  
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+         if (indexPath.section == 0) {
+        return 100
+         } else {
+        return 40
+        }
+    }
+    
+    
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
